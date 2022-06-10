c1a6b57d23a91fe0e72a8e1f6c8447206400991a
jakobeberhardt <jakob.eberhardt@mail.de>
2022-06-10 17:16:25 +0200

    remove sudo

--- client.sh
+++ client.sh
@@ -1,7 +1,7 @@
 # Setup instructions for a k6 benchmarking client along with an optional InfluxDB and Grafana dashboard
 # k6 for load testing https://github.com/grafana/k6
-sudo dnf install -y https://dl.k6.io/rpm/repo.rpm    
-sudo dnf install -y k6
+dnf install https://dl.k6.io/rpm/repo.rpm    
+dnf install k6
 
 # In case you plan to use a local InfluxDB https://www.influxdata.com/:
 cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo
@@ -13,14 +13,14 @@ gpgcheck = 1
 gpgkey = https://repos.influxdata.com/influxdb.key
 EOF
 
-sudo yum install influxdb
-sudo firewall-cmd --add-port=8086/tcp --permanent
-sudo firewall-cmd --reload
-sudo systemctl start influxdb
+dnf install influxdb
+firewall-cmd --add-port=8086/tcp --permanent
+firewall-cmd --reload
+systemctl start influxdb
 
 
 # In case you plan to run Grafana locally https://grafana.com/:
-sudo nano /etc/yum.repos.d/grafana.repo
+nano /etc/yum.repos.d/grafana.repo
 [grafana]
 name=grafana
 baseurl=https://packages.grafana.com/enterprise/rpm
@@ -31,8 +31,8 @@ gpgkey=https://packages.grafana.com/gpg.key
 sslverify=1
 sslcacert=/etc/pki/tls/certs/ca-bundle.crt
 
-sudo dnf install grafana
-sudo systemctl start grafana-server
-sudo systemctl enable grafana-server
-sudo firewall-cmd --add-port=3000/tcp --permanent
-sudo firewall-cmd --reload
+dnf install grafana
+systemctl start grafana-server
+systemctl enable grafana-server
+firewall-cmd --add-port=3000/tcp --permanent
+firewall-cmd --reload
